5aa4e9aad0e6b9ad968357b2d0cca0b57ba9e8f4
CrowdedAstronaut <ross@johnrossphillips.com>
2022-07-07 16:59:58 -0500

    Detail route is working, but wonky

--- src/CardDetails/CardDetails.js
+++ src/CardDetails/CardDetails.js
@@ -1,33 +1,46 @@
 import { useState, useEffect } from "react";
+// import { matchPath } from "react-router";
 
-export default function CardDetails({ match }) {
+export default function CardDetails() {
   const [anime, setAnimeDetails] = useState([]);
-
-  const getAnimeDetails = async () => {
-    const DETAILS_SEARCH = {
-      url: `https://kitsu.io/api/edge/anime/1`,
-    };
-    const detailsEndPoint = `${DETAILS_SEARCH.url}`;
-    try {
-      // fetch
-      const response = await fetch(detailsEndPoint);
-      const data = await response.json();
-      let data2 = Object.values(data)[0];
-      setAnimeDetails(data2);
-      console.log(data2.id);
-    } catch (err) {
-      console.log(err);
-    }
-  };
   useEffect(() => {
+    const getAnimeDetails = async () => {
+      const DETAILS_SEARCH = {
+        url: `https://kitsu.io/api/edge/anime/1`,
+      };
+      const detailsEndPoint = `${DETAILS_SEARCH.url}`;
+      try {
+        // fetch
+        const response = await fetch(detailsEndPoint);
+        const data = await response.json();
+        let data2 = Object.values(data)[0];
+        setAnimeDetails(data2);
+        // console.log(data2.id);
+        console.log(data2);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
     getAnimeDetails(anime);
-    console.log(match);
+    console.log(anime);
     // eslint-disable-next-line
   }, []);
 
   return (
     <div className="card-details">
-      <h1>{anime.attributes.canonicalTitle}</h1>
+      <div className="card-header">
+        <h1>{anime.attributes.canonicalTitle}</h1>
+        <div className="card-image">
+          <img
+            src={anime.attributes.coverImage.original}
+            alt=""
+          />
+        </div>
+        <p className="detail-description">
+          {anime.attributes.description}
+        </p>
+      </div>
     </div>
   );
 }
