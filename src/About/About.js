cb69c241dcabd950685b96a721bd40a2b5a44f61
CrowdedAstronaut <ross@johnrossphillips.com>
2022-08-21 12:34:07 -0500

    styled the about page

--- src/About/About.js
+++ src/About/About.js
@@ -1,13 +1,13 @@
 export default function About() {
   return (
-    <>
-      <h2>Welcome to All About Anime</h2>
+    <div className="about-wrapper">
+      <h2>Welcome!</h2>
       <p>
         This is a React Application that queries an Anime
         api and gives the user filter and search
         functionality. If the user clicks on a character,
         the app will return details about that character.
       </p>
-    </>
+    </div>
   );
 }
