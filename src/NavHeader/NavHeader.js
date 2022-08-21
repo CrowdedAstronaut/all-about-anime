69b44961766ef2360e7a9bda1358829d95dfe51d
CrowdedAstronaut <ross@johnrossphillips.com>
2022-08-21 12:07:56 -0500

    added 1200px boundary to all components

--- src/NavHeader/NavHeader.js
+++ src/NavHeader/NavHeader.js
@@ -4,14 +4,16 @@ import { Nav, Navbar } from "react-bootstrap";
 export default function NavHeader() {
   return (
     <nav>
-      <h2>
-        <a href="/">All About Anime</a>
-      </h2>
-      <ul className="nav-links">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/search">Search</Link>
-      </ul>
+      <div className="nav-wrapper">
+        <h2>
+          <a href="/">All About Anime</a>
+        </h2>
+        <ul className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/search">Search</Link>
+        </ul>
+      </div>
     </nav>
   );
 }
