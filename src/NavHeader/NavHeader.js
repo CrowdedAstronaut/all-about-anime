8336c0cd781f0bc9e3c0d3b4990c5ad7316196e7
CrowdedAstronaut <ross@johnrossphillips.com>
2022-08-21 18:32:07 -0500

    added react burger menu

--- src/NavHeader/NavHeader.js
+++ src/NavHeader/NavHeader.js
@@ -1,14 +1,22 @@
 import { Link } from "react-router-dom";
-import { Nav, Navbar } from "react-bootstrap";
+import { useState } from "react";
+import { slide as Menu } from "react-burger-menu";
 
 export default function NavHeader() {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const handleToggle = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
   return (
     <nav>
       <div className="nav-wrapper">
         <h1>
           <a href="/">All About Anime</a>
         </h1>
-        <ul className="nav-links">
+
+        <button onClick={handleToggle}>{navbarOpen ? "Close" : "Open"}</button>
+        <ul className={`menuNav ${navbarOpen ? " showMenu" : ""}`}>
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
           <Link to="/search">Search</Link>
