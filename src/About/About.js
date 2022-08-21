8b54baad56c37aee940f30eb360ce99fb6972a5a
CrowdedAstronaut <ross@johnrossphillips.com>
2022-08-21 17:54:24 -0500

    added a google font for paragraph

--- src/About/About.js
+++ src/About/About.js
@@ -3,10 +3,15 @@ export default function About() {
     <div className="about-wrapper">
       <h2>Welcome!</h2>
       <p>
-        This is a React Application that queries an Anime
-        api and gives the user filter and search
-        functionality. If the user clicks on a character,
-        the app will return details about that character.
+        Anime (Japanese: アニメ, IPA: [aɲime]) is hand-drawn and
+        computer-generated animation originating from Japan. Outside of Japan
+        and in English, anime refers specifically to animation produced in
+        Japan.
+      </p>
+      <p>
+        This React Application queries an Anime api and gives the user filter
+        and search functionality. If the user clicks on a character, the the app
+        will return details about that character.
       </p>
     </div>
   );
